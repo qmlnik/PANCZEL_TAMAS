84fb9ba6b11f86d674d343fb6a28b842e51e6530
Panczel Andris <panczeland@gmail.com>
2025-03-21 10:12:41 +0100

    felesleges nyelvesites torles

--- i18n.config.ts
+++ i18n.config.ts
@@ -46,55 +46,5 @@ export default defineI18nConfig(() => ({
             }
         }
     },
-    /*locales: [
-        {
-        code: "hu",
-        messages: {
-            showMore: "Többet",
-            showLess: "Kevesebbet",
-            languageSelect: "Nyelv választása",
-            hero: {
-                title: "pánczél tamás",
-                subtitle: "Hegedűművész. Zeneszerző.",
-                introduction: {
-                    title: "bemutatkozás",
-                    subtitle: "Minden hangot a szenvedély vezet",
-                    content: "Lórum ipse száran, jogán, de a medó besítő, a főzés pedig egészen gyűjtő pali. A válka tömbjére üvözölte: a bibecske csakis a ferges kozás zölését kodja bőgőnek, mégpedig szigorúan a korábban boros nésetlevények között. Jelletesség handék adódta: a bibecske nem feselyedi a jáló foránok bűnök, de mert a szajlomta tapált kozás szerdes lecskenyelése a ferges szenség lenne, és mert a mozás részéről erre nem ingol mozmányos."
-                }
-            },
-            categories: "Kategóriák",
-            footer: {
-                address: "Cím",
-                phone: "Telefon",
-                email: "Email",
-                connect: "Kapcsolat"
-            }
-        }
-        },
-        {
-        code: "en",
-        messages: {
-            showMore: "Show more",
-            showLess: "Show less",
-            languageSelect: "Select language",
-            hero: {
-                title: "tamas panczel",
-                subtitle: "Violinist. Composer. Conductor",
-                introduction: {
-                    title: "introduction",
-                    subtitle: "Passion drives every note",
-                    content: "At the heart of this extraordinary artist\"s musical journey lies a profound and unwavering motivation, one that has been the driving force behind every note played, composed, and conducted. The motivation is not merely personal accomplishment or recognition, but the genuine desire to touch the lives of others through the universal language of music, forging emotional connections that transcend boundaries and time itself."
-                }
-            },
-            categories: "Categories",
-            footer: {
-                address: "Address",
-                phone: "Phone",
-                email: "Email",
-                connect: "Connect"
-            }
-        }
-        }
-    ],*/
     defaultLocale: "en"
 }))
